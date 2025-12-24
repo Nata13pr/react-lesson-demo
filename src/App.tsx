96967bea1df038fd70c8cf336f50d47b4d65482c
nata13pr <nata13pr@gmail.com>
2025-12-24 10:24:46 +0200

    add lesson1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className="text-3xl font-bold underline">
+            Hello Okten
+        </div>
+    )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+})
