dbdd94cd3c39cd69aba2eef850acac94cf0abf97
nata13pr <nata13pr@gmail.com>
2025-12-24 13:42:39 +0200

    add homework1_3

--- src/components/character-component/CharacterComponent.tsx
+++ src/components/character-component/CharacterComponent.tsx
@@ -1,6 +1,7 @@
 import {type ISimpsonProps} from "../../models/ICharacter.ts";
 
 const CharacterComponent = ({simpson, children}: ISimpsonProps) => {
+
     return (
         <li className={"bg-[lightgrey]"}>
             <h2 className="uppercase underline decoration-dotted text-2xl ">{simpson.name}-{simpson.surname}.Вік-{simpson.age}</h2>
