d1a4546a97d795bf7e50af294e8f8c7982b842d2
nata13pr <nata13pr@gmail.com>
2025-12-30 20:50:44 +0200

    add homework2_2

--- src/components/post-component/PostComponent.tsx
+++ src/components/post-component/PostComponent.tsx
@@ -8,7 +8,7 @@ type PostPropType = {
 const PostComponent: FC<PostPropType> = ({post}) => {
     return (
         <li>
-            <h2>User ID - {post.userId}.Task number - {post.id}</h2>
+            <h2>Post ID - {post.userId}.Post number - {post.id}</h2>
             <h3>Title - {post.title}</h3>
             <p>Body - {post.body}</p>
         </li>
