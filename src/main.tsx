d91050836e0eaf557c0a4bd5d40a5df3368e22a9
nata13pr <nata13pr@gmail.com>
2026-01-04 23:57:30 +0200

    add homework3_1_browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,23 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import { RouterProvider} from "react-router";
-import {routes} from "./router/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router";
+import MainLayout from "./layouts/MainLayout.tsx";
+import UsersPage from "./pages/UsersPage.tsx";
+import CommentsPage from "./pages/CommentsPage.tsx";
+import PostsPage from "./pages/PostsPage.tsx";
+import ProductsPage from "./pages/ProductsPage.tsx";
 
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>)
+    .render(
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<MainLayout/>}>
+                    <Route path={'users'} element={<UsersPage/>}/>
+                    <Route path={'comments'} element={<CommentsPage/>}/>
+                    <Route path={'posts'} element={<PostsPage/>}/>
+                    <Route path={'products'} element={<ProductsPage/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
