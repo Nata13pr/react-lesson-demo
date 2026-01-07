29056af8aae0f81d485585e001657e8fe9a5759c
nata13pr <nata13pr@gmail.com>
2026-01-07 12:37:20 +0200

    add homework3_3_router object

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,26 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from 'react-router'
-import {routes} from "./router/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router";
+import MainLayout from "./layouts/MainLayout.tsx";
+import UsersDummyjsonPage from "./pages/UsersDummyjsonPage.tsx";
+import UsersJsonplaceholderPage from "./pages/UsersJsonplaceholderPage.tsx";
+import PostsDummyjsonPage from "./pages/PostsDummyjsonPage.tsx";
+import PostsJsonplaceholderPage from "./pages/PostsJsonplaceholderPage.tsx";
+import CommentsDummyjsonPage from "./pages/CommentsDummyjsonPage.tsx";
+import CommentsJsonplaceholderPage from "./pages/CommentsJsonplaceholderPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={routes}/>
+    <BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route path={'users/dummyjson'} element={<UsersDummyjsonPage/>}/>
+                <Route path={'users/jsonplaceholder'} element={<UsersJsonplaceholderPage/>}/>
+                <Route path={'posts/dummyjson'} element={<PostsDummyjsonPage/>}/>
+                <Route path={'posts/jsonplaceholder'} element={<PostsJsonplaceholderPage/>}/>
+                <Route path={'comments/dummyjson'} element={<CommentsDummyjsonPage/>}/>
+                <Route path={'comments/jsonplaceholder'} element={<CommentsJsonplaceholderPage/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
 )
+
