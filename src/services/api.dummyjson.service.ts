2e6b9704669d86af9dfa5ed5c1ecb71412b9f70c
nata13pr <nata13pr@gmail.com>
2026-01-07 13:20:59 +0200

    add homework3_3_browser-router

--- src/services/api.dummyjson.service.ts
+++ src/services/api.dummyjson.service.ts
@@ -1,4 +1,6 @@
 import type {IUserDummyjson} from "../models/dummyjson-user-model/IUserDummyjson.ts";
+import type {IPostDummyjson} from "../models/IPostDummyjson.ts";
+import type {ICommentDummyjson} from "../models/ICommentDummyjson.ts";
 
 const endpointUserDummyson = import.meta.env.VITE_API_BASE_URL_DUMMYJSON + '/users';
 const endpointPostsDummyson = import.meta.env.VITE_API_BASE_URL_DUMMYJSON + '/posts';
@@ -11,13 +13,13 @@ const loadDummysonUsers = async (): Promise<IUserDummyjson[]> => {
     return response.users;
 }
 
-const loadDummysonPosts = async () => {
+const loadDummysonPosts = async ():Promise<IPostDummyjson[]> => {
     const response = await fetch(endpointPostsDummyson)
         .then(res => res.json());
     return response.posts;
 }
 
-const loadDummysonComments = async () => {
+const loadDummysonComments = async ():Promise<ICommentDummyjson[]> => {
     const response = await fetch(endpointCommentsDummyson)
         .then(res => res.json());
     return response.comments;
