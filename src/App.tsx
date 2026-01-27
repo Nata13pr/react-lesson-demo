8d4213c2e32296badaf595ee81fd6bdc6cf976cb
nata13pr <nata13pr@gmail.com>
2026-01-27 22:35:12 +0200

    add homework7_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import {UsersComponent} from "./components/UsersCompoent.tsx";
+
 const App = () => {
     return (
         <>
-            <div>Hello</div>
+           <UsersComponent/>
         </>
     )
 }
