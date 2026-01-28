8b5c7e9e20e1a360af5214c8f53f6022b621a0ba
nata13pr <nata13pr@gmail.com>
2026-01-28 12:33:51 +0200

    add homework7

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import CommentsComponent from "./components/comments-component/CommentsComponent.tsx";
-import PostsComponent from "./components/posts-component/PostsComponent.tsx";
-import TodosComponent from "./components/todos-component/TodosComponent.tsx";
-
 const App = () => {
     return (
-        <>
-            <CommentsComponent/>
-            <PostsComponent/>
-            <TodosComponent/>
-        </>
+        <div>
+        Hello okten
+        </div>
     )
 }
 export default App;
--- src/models/IResponseModel.ts
+++
@@ -1,7 +0,0 @@
-export type IResponseModel<T, K extends string> = {
-    total: number;
-    skip: number;
-    limit: number;
-} & {
-    [P in K]: T[];
-}
